69a238978f3c89c05c9adf0542d41abfc66d6119
sapta07-cg <chakrabortysaptarshi03@gmail.com>
2021-05-17 10:59:33 +0530

    erroe

--- src/main/java/com/capgemini/controller/LADController.java
+++ src/main/java/com/capgemini/controller/LADController.java
@@ -1,5 +1,11 @@
 package com.capgemini.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lad/")
 public class LADController {
+	System.out.println("hello");
 
 }
